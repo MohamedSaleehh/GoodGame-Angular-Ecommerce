b23640ba2fb2c1f01d7ff6c6a179cd40ffdda873
nihal-gamal <86268645+nihal-gamal@users.noreply.github.com>
2022-08-05 17:43:11 +0200

    plus button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{useHash:true})],
+  imports: [RouterModule.forRoot(routes,{scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
